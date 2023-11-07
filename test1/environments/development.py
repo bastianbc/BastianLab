8da00884d3c82e22ec6353fc206e287a46d1c571
ceylanbagci <hotbird3005@hotmail.com>
2023-11-07 07:42:15 -0800

    dev

--- test1/environments/development.py
+++ test1/environments/development.py
@@ -127,9 +127,9 @@ WSGI_APPLICATION = 'test1.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'USER': 'gerboyto',
-        'PASSWORD': '3214',
-        'NAME': 'bastian_lab',
+        'USER': 'testuser',
+        'PASSWORD': '1235',
+        'NAME': 'labdb',
         'HOST': '127.0.0.1',
         'PORT': '5432',
         'DISABLE_SERVER_SIDE_CURSORS': True
