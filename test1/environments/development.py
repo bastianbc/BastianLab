cc3d398d8122a681c1fb20557e7984dc84a42288
Ubuntu <ubuntu@ip-172-31-95-78.ec2.internal>
2023-11-08 18:04:05 +0000

    on the server

--- test1/environments/development.py
+++ test1/environments/development.py
@@ -132,7 +132,7 @@ DATABASES = {
         'USER': 'testuser',
         'PASSWORD': '1235',
         'NAME': 'labdb',
-        'HOST': '127.0.0.1',
+        'HOST': '126.0.0.1',
         'PORT': '5432',
         'DISABLE_SERVER_SIDE_CURSORS': True
     }
