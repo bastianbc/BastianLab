a6dbc5236f395b365114a317d8393fd9f807b3c3
ceylanbagci <hotbird3005@hotmail.com>
2023-11-21 13:22:42 -0800

    projects fixed

--- capturedlib/models.py
+++ capturedlib/models.py
@@ -121,7 +121,8 @@ class CapturedLib(models.Model):
 
     def _set_nm(self):
         # Calculate CL.nM as CL.conc/660 * CL.frag_size * 10^6 and store in CL.nM
-        self.nm = round(self.conc/(660 * float(self.frag_size)) * 10**6,2)
+        # self.nm = round(self.conc/(660 * float(self.frag_size)) * 10**6,2)
+        pass
 
     def update_volume(self, volume):
         self.vol_remain = 0 if volume > self.vol_remain else round(self.vol_remain - volume,2)
@@ -131,7 +132,7 @@ class SL_CL_LINK(models.Model):
     captured_lib = models.ForeignKey("capturedlib.CapturedLib",on_delete=models.CASCADE, verbose_name="Captured Library", related_name="sl_cl_links")
     sample_lib = models.ForeignKey("samplelib.SampleLib", on_delete=models.CASCADE, verbose_name="Sample Library", related_name="sl_cl_links")
     volume = models.FloatField(default=0, verbose_name="Volume")
-    amount = models.FloatField(default=0, verbose_name="Input Amount")
+    amount = models.FloatField(default=0, verbose_name="Amount")
 
     class Meta:
         db_table = "sl_cl_link"
