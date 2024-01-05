93eb543c675a13dce0ba435363497e3edaf21882
ceylanbagci <hotbird3005@hotmail.com>
2024-01-05 08:42:05 -0800

    working on sequencingruns

--- test1/environments/development.py
+++ test1/environments/development.py
@@ -181,7 +181,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 
-STATIC_URL = '/staticfiles/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'test1/static'),
 ]
