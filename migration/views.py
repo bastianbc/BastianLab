5bec4210403072a64b93598555eeb7c139630fb6
ceylanbagci <hotbird3005@hotmail.com>
2024-01-18 16:58:31 -0800

    working on nucleic acids

--- migration/views.py
+++ migration/views.py
@@ -3153,10 +3153,10 @@ def check_projects_airtable_get(row):
 
 def check_projects_airtable(request):
     file = Path(Path(__file__).parent.parent / "uploads" / "Patients-Grid view (1).csv")
-    file = Path(Path(__file__).parent.parent / "uploads" / "Blocks-Grid view (2).csv")
     file = Path(Path(__file__).parent.parent / "uploads" / "Areas-Grid view (3).csv")
     file = Path(Path(__file__).parent.parent / "uploads" / "Nucleic Acids-Grid view (2).csv")
     file = Path(Path(__file__).parent.parent / "uploads" / "Sample Library with grid view, analysis view and more-Grid view (5).csv")
+    file = Path(Path(__file__).parent.parent / "uploads" / "Blocks-Grid view (3).csv")
     df = pd.read_csv(file)
     df[~df["Assigned Projects"].isnull()].apply(lambda row: check_projects_airtable_get(row), axis=1)
 
