732dce4942303823b1fe1e28fc2d2762538e5e83
bazo <sitivcabs@yahoo.com>
2024-02-18 13:32:26 +0100

    since pull last code after long time

--- blocks/views.py
+++ blocks/views.py
@@ -9,15 +9,10 @@ from projects.models import Projects
 from .serializers import BlocksSerializer
 from django.contrib.auth.decorators import login_required,permission_required
 from core.decorators import permission_required_for_async
-import pandas as pd
-from pathlib import Path
 from method.models import Method
-
 
 @permission_required_for_async("blocks.view_blocks")
 def filter_blocks(request):
-    from .serializers import BlocksSerializer
-    # _create_blocks_from_file()
     blocks = Blocks.query_by_args(request.user,**request.GET)
     serializer = BlocksSerializer(blocks['items'], many=True)
     result = dict()
@@ -25,7 +20,7 @@ def filter_blocks(request):
     result['draw'] = blocks['draw']
     result['recordsTotal'] = blocks['total']
     result['recordsFiltered'] = blocks['count']
-    print(result['data'])
+    
     return JsonResponse(result)
 
 @permission_required("blocks.view_blocks",raise_exception=True)
@@ -219,188 +214,3 @@ def edit_block_url(request):
     else:
         form = BlockUrlForm(instance=instance)
     return render(request,"block_url.html",locals())
-
-
-def _assign_projects_to_blocks():
-    from pyairtable import Api
-    api = Api('keyEDswuVpUGOz8Tp')
-    t = api.table("appA7qA5hhuLgiAwt", "tblv5WQXW7cNCbt0o")
-    for i in t.all():
-        for s in i.get("fields").get("Assigned project", []):
-            try:
-                p = Projects.objects.get(name=s.strip())
-                b = Blocks.objects.get(name=i.get("fields").get("Block_ID", ""))
-            except Exception as e:
-                print(i.get("fields").get("Block_ID", ""))
-                print(e)
-            b.project = p
-            b.save()
-            print("saved")
-
-
-def _assign_patients_to_blocks():
-    from pyairtable import Api
-    api = Api('keyEDswuVpUGOz8Tp')
-    pat_table = api.table("appA7qA5hhuLgiAwt", "tblxWxK1fH2VMbI4Q")
-    block_table = api.table("appA7qA5hhuLgiAwt", "tblv5WQXW7cNCbt0o")
-
-    for pat in pat_table.all():
-        for block in pat.get("fields").get("Blocks _ID", []):
-
-            try:
-                bb = block_table.get(block).get("fields").get("Block_ID", "")
-
-                p = Patients.objects.get(pat_id=pat.get("fields").get("Pat_ID", []))
-                b = Blocks.objects.get(name=bb)
-                b.patient = p
-                b.save()
-
-            except Exception as e:
-                print(pat.get("fields").get("Block_ID", ""))
-                print(e)
-
-def get_pat_id(pk):
-    from pyairtable import Api
-    api = Api('keyEDswuVpUGOz8Tp')
-    pat_table = api.table("appA7qA5hhuLgiAwt", "tblxWxK1fH2VMbI4Q")
-    try:
-        res = pat_table.get(pk[0])
-        p = Patients.objects.get(pat_id=res.get("fields").get("Pat_ID"))
-        return p
-    except Exception as e:
-        print(e)
-
-
-def get_project_id(pk):
-    from pyairtable import Api
-    api = Api('keyEDswuVpUGOz8Tp')
-    pro_table = api.table("appA7qA5hhuLgiAwt", "tblT4u7Cgb4fUgaiI")
-    try:
-        res = pro_table.get(pk[0])
-        p = Projects.objects.get(name=res.get("fields").get("Assigned Project"))
-        return p
-    except Exception as e:
-        print(e)
-
-
-def get_collection(value):
-    for x in Blocks.COLLECTION_CHOICES:
-        if value.lower() == x[1].lower():
-            return x[0]
-    return Blocks.SCRAPE
-
-def get_p_stage(value):
-    for x in Blocks.P_STAGE_TYPES:
-        if value.lower() == x[1].lower():
-            return x[0]
-    return
-
-def get_prim(value):
-    for x in Blocks.PRIM_TYPES:
-        if value.lower() == x[1].lower():
-            return x[0]
-    return
-
-
-def get_or_create_blocks(fields:dict):
-    try:
-        block = Blocks.objects.get(name=fields.get("Block_ID"))
-        if fields.get("Pat_ID", []):
-            block.patient = get_pat_id(fields.get("Pat_ID",""))
-        if fields.get("Assigned Project",[]):
-            block.project = get_project_id(fields.get("Assigned Project",[]))
-        block.slides = fields.get("Slides",None)
-        block.slides_left = fields.get("Slides left",None)
-        block.fixation = fields.get("Fixation",None)
-        block.collection = get_collection(fields.get("Collection Method",""))
-        block.diagnosis = fields.get("Diagnosis",None)
-        block.save()
-        print("saved")
-    except Exception as e:
-        print(e)
-
-
-def _create_blocks_airtable():
-    from pyairtable import Api
-    api = Api('keyEDswuVpUGOz8Tp')
-    t = api.table("appA7qA5hhuLgiAwt", "tblv5WQXW7cNCbt0o")
-    for i in t.all():
-        get_or_create_blocks(i.get("fields"))
-
-
-def _pat_get_or_create_consolidated(row):
-    try:
-        # print(row)
-        b = Blocks.objects.filter(name=row["Block"]).update(
-            age=None if pd.isnull(row["pat_age"] ) else float(row["pat_age"]),
-            thickness=row["thickness"] or "",
-            mitoses=None if pd.isnull(row["mitoses"]) else int(row["mitoses"]),
-            p_stage=row["p_stage"] or "",
-            prim=row["prim"] or "",
-            subtype=row["subtype"] or "",
-        )
-        # b.notes = row["Notes/Other"]
-        # b.save()
-        print("saved")
-    except Exception as e:
-        print(e)
-
-def _create_blocks_consolidated_data():
-    import pandas as pd
-    from pathlib import Path
-    file = Path(Path(__file__).parent.parent / "uploads" / "Consolidated_data_final.csv")
-    df = pd.read_csv(file)
-    df.apply(lambda row: _pat_get_or_create_consolidated(row), axis=1)
-
-def get_or_create_blocks_from_file(row):
-    try:
-        print(row["Block_ID"])
-        ulcreation = True if row["Ulceration"] == "Present" else False if row["Ulceration"] == "negative" else None
-        Blocks.objects.filter(name=row["Block_ID"]).update(
-            fixation=row["Fixation"],
-            slides=int(row["Slides"]) if not pd.isnull(row["Slides"]) else None,
-            slides_left=int(row["Slides left"]) if not pd.isnull(row["Slides left"]) else None,
-            ulceration=ulcreation,
-            collection=get_collection(row["Collection Method"]) if not pd.isnull(row["Collection Method"]) else ""
-        )
-        print("saved")
-    except Exception as e:
-        print("error"*10,e)
-
-
-def get_or_create_bl(row):
-    # try:
-    print(row["name"])
-    try:
-        Blocks.objects.create(name=row["name"])
-    except:
-        print("")
-    Blocks.objects.filter(name=row["name"]).update(
-        age=row["pat_age"],
-        thickness=row["thickness"],
-        mitoses=int(row["mitoses"]) if not pd.isnull(row["mitoses"]) else None,
-        p_stage=get_p_stage(row["p_stage"]) if not pd.isnull(row["p_stage"]) else None,
-        prim=get_p_stage(row["prim"]) if not pd.isnull(row["prim"]) else None,
-        subtype=row["subtype"],
-        diagnosis=row["dx_text"],
-        notes=row["note"],
-        micro=row["micro"],
-        path_note=row["Path Number"]
-    )
-    print("saved")
-    # except Exception as e:
-    #     print("error"*10,e)
-
-
-
-def _create_blocks_from_file():
-    file = Path(Path(__file__).parent.parent / "uploads" / "Blocks-Grid view-5.csv")
-    df = pd.read_csv(file)
-    df.apply(lambda row: get_or_create_blocks_from_file(row), axis=1)
-
-#
-# def _create_blocks_from_file():
-#     file = Path(Path(__file__).parent.parent / "uploads" / "report-block.csv")
-#     df = pd.read_csv(file)
-#     df = df.apply(lambda row: get_or_create_bl(row), axis=1)
-#     df.to_csv("report_matching_sample_lib.csv", index=False)
