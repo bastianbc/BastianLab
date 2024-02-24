0707a666d7370c5062a83fb5afc8422d97655593
ceylanbagci <hotbird3005@hotmail.com>
2024-02-24 13:30:52 -0800

    working on nucleic acids

--- sequencingrun/static/sequencingrun.js
+++ sequencingrun/static/sequencingrun.js
@@ -901,7 +901,7 @@ var KTDatatablesServerSide = function () {
                       <div class="col-2">${sel.outerHTML}</div>
                       <div class="col-4"><input type="text" class="form-control fset form-control-sm" value="${data.files[i][2]}"></div>
                       <div class="col-4 d-flex justify-content-center">
-                        <span class="fname" style="max-width: 100%; overflow: hidden; text-overflow: ellipsis; white-space: nowrap;">
+                        <span class="fname text-start" style="max-width: 100%; overflow: hidden; text-overflow: ellipsis; white-space: nowrap;">
                             ${data.files[i][0]}
                         </span>
                       </div>
