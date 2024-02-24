9451859fd25287cbf974315a8ce3d8c8daf52dee
ceylanbagci <hotbird3005@hotmail.com>
2024-02-23 18:55:31 -0800

    working on nucleic acids

--- sequencingrun/views.py
+++ sequencingrun/views.py
@@ -319,8 +319,7 @@ def save_sequencing_files(request):
 
             shutil.move(source_file, destination_file)
 
-        success = True
-        return JsonResponse({"result": success}, status=200)
+        return JsonResponse({"result": True}, status=200)
     except Exception as e:
         print(e)
         return JsonResponse({"error":str(e)}, status=400)
