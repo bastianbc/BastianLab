5752dd06dc8c5f9083179f77d1fd236f1bcc8b88
ceylanbagci <hotbird3005@hotmail.com>
2024-02-28 09:55:51 -0800

    working on nucleic acids

--- sequencingrun/views.py
+++ sequencingrun/views.py
@@ -413,7 +413,7 @@ def save_sequencing_files(request):
                 destination_file = os.path.join(destination_dir, filename)
                 # cmd = ['sudo', 'mv', source_file, destination_file]
                 # subprocess.run(cmd, check=True)
-                shutil.move(source_file, destination_dir)
+                shutil.move(source_file, destination_file)
             # print("source_file: %s destination_file: %s" %(source_file, destination_file))
             # executor.submit(shutil.move(source_file, destination_file))
             except Exception as e:
