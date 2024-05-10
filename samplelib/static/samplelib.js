1050c17d0fba9aee633ce766861787383cd28a84
ceylanbagci <hotbird3005@hotmail.com>
2024-05-10 07:33:37 -0700

    sheet development

--- samplelib/static/samplelib.js
+++ samplelib/static/samplelib.js
@@ -63,24 +63,24 @@ var KTDatatablesServerSide = function () {
             columns: [
                 { data: 'id' },
                 { data: 'name' },
-                { data: 'area_num' },
-                { data: 'date',
-                  render: function (data) {
-                    return moment(data).format('MM/DD/YYYY');
-                  }
-                },
-                { data: 'num_files' },
-                { data: 'qpcr_conc' },
-                { data: 'vol_init' },
-                { data: 'amount_final' },
-                { data: 'pcr_cycles' },
-                { data: 'qubit' },
-                { data: 'amount_in' },
-                { data: 'vol_remain' },
-                { data: 'num_blocks' },
-                { data: 'num_nucacids' },
-                // { data: 'num_capturedlibs' },
-                { data: 'barcode' },
+                // { data: 'area_num' },
+                // { data: 'date',
+                //   render: function (data) {
+                //     return moment(data).format('MM/DD/YYYY');
+                //   }
+                // },
+                // { data: 'num_files' },
+                // { data: 'qpcr_conc' },
+                // { data: 'vol_init' },
+                // { data: 'amount_final' },
+                // { data: 'pcr_cycles' },
+                // { data: 'qubit' },
+                // { data: 'amount_in' },
+                // { data: 'vol_remain' },
+                // { data: 'num_blocks' },
+                // { data: 'num_nucacids' },
+                // // { data: 'num_capturedlibs' },
+                // { data: 'barcode' },
             ],
             columnDefs: [
                 {
@@ -94,54 +94,54 @@ var KTDatatablesServerSide = function () {
                             </div>`;
                     }
                 },
-                {
-                    targets: 2,
-                    orderable: true,
-                    render: function (data, type, row) {
-                        if (data > 0) {
-                          let sl_id = row["id"];
-                          return `
-                              <a href="/areas?model=sample_lib&id=${sl_id}&initial=true">${data}</a>`;
-                        }
-                        return data;
-                    }
-                },
-                {
-                    targets: 4,
-                    orderable: true,
-                    render: function (data, type, row) {
-                        if (data > 0) {
-                          let sl_id = row["id"];
-                          return `
-                              <a href="/sequencingfile?id=${sl_id}&initial=true&model=sample_lib">${data}</a>`;
-                        }
-                        return data;
-                    }
-                },
-                {
-                    targets: 12,
-                    orderable: false,
-                    render: function (data, type, row) {
-                        if (data > 0) {
-                          let id = row["id"];
-                          return `
-                              <a href="/blocks?model=samplelib&id=${id}&initial=true">${data}</a>`;
-                        }
-                        return data;
-                    }
-                },
-                {
-                    targets: 13,
-                    orderable: false,
-                    render: function (data, type, row) {
-                        if (data > 0) {
-                          let id = row["id"];
-                          return `
-                              <a href="/libprep?model=sample_lib&id=${id}&initial=true">${data}</a>`;
-                        }
-                        return data;
-                    }
-                },
+                // {
+                //     targets: 2,
+                //     orderable: true,
+                //     render: function (data, type, row) {
+                //         if (data > 0) {
+                //           let sl_id = row["id"];
+                //           return `
+                //               <a href="/areas?model=sample_lib&id=${sl_id}&initial=true">${data}</a>`;
+                //         }
+                //         return data;
+                //     }
+                // },
+                // {
+                //     targets: 4,
+                //     orderable: true,
+                //     render: function (data, type, row) {
+                //         if (data > 0) {
+                //           let sl_id = row["id"];
+                //           return `
+                //               <a href="/sequencingfile?id=${sl_id}&initial=true&model=sample_lib">${data}</a>`;
+                //         }
+                //         return data;
+                //     }
+                // },
+                // {
+                //     targets: 12,
+                //     orderable: false,
+                //     render: function (data, type, row) {
+                //         if (data > 0) {
+                //           let id = row["id"];
+                //           return `
+                //               <a href="/blocks?model=samplelib&id=${id}&initial=true">${data}</a>`;
+                //         }
+                //         return data;
+                //     }
+                // },
+                // {
+                //     targets: 13,
+                //     orderable: false,
+                //     render: function (data, type, row) {
+                //         if (data > 0) {
+                //           let id = row["id"];
+                //           return `
+                //               <a href="/libprep?model=sample_lib&id=${id}&initial=true">${data}</a>`;
+                //         }
+                //         return data;
+                //     }
+                // },
                 // {
                 //     targets: 14,
                 //     orderable: false,
@@ -154,54 +154,54 @@ var KTDatatablesServerSide = function () {
                 //         return data;
                 //     }
                 // },
-                {
-                    targets: 15,
-                    data: null,
-                    orderable: false,
-                    className: 'text-end',
-                    render: function (data, type, row) {
-                        return `
-                            <a href="#" class="btn btn-light btn-active-light-primary btn-sm" data-kt-menu-trigger="click" data-kt-menu-placement="bottom-end" data-kt-menu-flip="top-end">
-                                Actions
-                                <span class="svg-icon svg-icon-5 m-0">
-                                    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
-                                        <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                                            <polygon points="0 0 24 0 24 24 0 24"></polygon>
-                                            <path d="M6.70710678,15.7071068 C6.31658249,16.0976311 5.68341751,16.0976311 5.29289322,15.7071068 C4.90236893,15.3165825 4.90236893,14.6834175 5.29289322,14.2928932 L11.2928932,8.29289322 C11.6714722,7.91431428 12.2810586,7.90106866 12.6757246,8.26284586 L18.6757246,13.7628459 C19.0828436,14.1360383 19.1103465,14.7686056 18.7371541,15.1757246 C18.3639617,15.5828436 17.7313944,15.6103465 17.3242754,15.2371541 L12.0300757,10.3841378 L6.70710678,15.7071068 Z" fill="currentColor" fill-rule="nonzero" transform="translate(12.000003, 11.999999) rotate(-180.000000) translate(-12.000003, -11.999999)"></path>
-                                        </g>
-                                    </svg>
-                                </span>
-                            </a>
-                            <!--begin::Menu-->
-                            <div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-600 menu-state-bg-light-primary fw-bold fs-7 w-150px py-4" data-kt-menu="true">
-                                <!--begin::Menu item-->
-                                <div class="menu-item px-3">
-                                    <a href="/samplelib/edit/`+ row["id"] +`" class="menu-link px-3" data-kt-docs-table-filter="edit_row">
-                                        Edit
-                                    </a>
-                                </div>
-                                <!--end::Menu item-->
-
-                                <!--begin::Menu item-->
-                                <div class="menu-item px-2">
-                                    <a href="javascript:;" class="menu-link px-3 detail-link" data-kt-docs-table-filter="detail_row">
-                                        Used Nucacids(s)
-                                    </a>
-                                </div>
-                                <!--end::Menu item-->
-
-                                <!--begin::Menu item-->
-                                <div class="menu-item px-3">
-                                    <a href="/samplelib/delete/` + row["id"] +`" class="menu-link px-3" data-kt-docs-table-filter="delete_row">
-                                        Delete
-                                    </a>
-                                </div>
-                                <!--end::Menu item-->
-                            </div>
-                            <!--end::Menu-->
-                        `;
-                    },
-                },
+                // {
+                //     targets: 15,
+                //     data: null,
+                //     orderable: false,
+                //     className: 'text-end',
+                //     render: function (data, type, row) {
+                //         return `
+                //             <a href="#" class="btn btn-light btn-active-light-primary btn-sm" data-kt-menu-trigger="click" data-kt-menu-placement="bottom-end" data-kt-menu-flip="top-end">
+                //                 Actions
+                //                 <span class="svg-icon svg-icon-5 m-0">
+                //                     <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+                //                         <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                //                             <polygon points="0 0 24 0 24 24 0 24"></polygon>
+                //                             <path d="M6.70710678,15.7071068 C6.31658249,16.0976311 5.68341751,16.0976311 5.29289322,15.7071068 C4.90236893,15.3165825 4.90236893,14.6834175 5.29289322,14.2928932 L11.2928932,8.29289322 C11.6714722,7.91431428 12.2810586,7.90106866 12.6757246,8.26284586 L18.6757246,13.7628459 C19.0828436,14.1360383 19.1103465,14.7686056 18.7371541,15.1757246 C18.3639617,15.5828436 17.7313944,15.6103465 17.3242754,15.2371541 L12.0300757,10.3841378 L6.70710678,15.7071068 Z" fill="currentColor" fill-rule="nonzero" transform="translate(12.000003, 11.999999) rotate(-180.000000) translate(-12.000003, -11.999999)"></path>
+                //                         </g>
+                //                     </svg>
+                //                 </span>
+                //             </a>
+                //             <!--begin::Menu-->
+                //             <div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-600 menu-state-bg-light-primary fw-bold fs-7 w-150px py-4" data-kt-menu="true">
+                //                 <!--begin::Menu item-->
+                //                 <div class="menu-item px-3">
+                //                     <a href="/samplelib/edit/`+ row["id"] +`" class="menu-link px-3" data-kt-docs-table-filter="edit_row">
+                //                         Edit
+                //                     </a>
+                //                 </div>
+                //                 <!--end::Menu item-->
+                //
+                //                 <!--begin::Menu item-->
+                //                 <div class="menu-item px-2">
+                //                     <a href="javascript:;" class="menu-link px-3 detail-link" data-kt-docs-table-filter="detail_row">
+                //                         Used Nucacids(s)
+                //                     </a>
+                //                 </div>
+                //                 <!--end::Menu item-->
+                //
+                //                 <!--begin::Menu item-->
+                //                 <div class="menu-item px-3">
+                //                     <a href="/samplelib/delete/` + row["id"] +`" class="menu-link px-3" data-kt-docs-table-filter="delete_row">
+                //                         Delete
+                //                     </a>
+                //                 </div>
+                //                 <!--end::Menu item-->
+                //             </div>
+                //             <!--end::Menu-->
+                //         `;
+                //     },
+                // },
             ],
             // Add data-filter attribute
             createdRow: function (row, data, dataIndex) {
