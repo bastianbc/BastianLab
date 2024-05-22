cca586db47a1f863c9f790f02b1f4cf653e50a8c
ceylanbagci <hotbird3005@hotmail.com>
2024-05-22 07:27:43 -0700

    sheets development

--- sheet/views.py
+++ sheet/views.py
@@ -56,7 +56,7 @@ def filter_sheet(request):
        # ),
     ).distinct().order_by('name')
     for i in q:
-        print(i)
+        print(i.__dict__)
     seq_runs = SequencingRun.objects.filter()
     samplelibs = query_by_args(request.user, seq_runs, **request.GET)
     serializer = CustomSampleLibSerializer(samplelibs['items'], many=True)
