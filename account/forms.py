e942bbad2d59292139a73a4ecdf18b3113fd0d07
ceylanbagci <hotbird3005@hotmail.com>
2024-05-28 17:06:28 -0700

    sheets development

--- account/forms.py
+++ account/forms.py
@@ -1,5 +1,8 @@
 from django import forms
 from django.contrib.auth import get_user_model
+from django.db import transaction
+from django.db.models import Max
+
 
 User = get_user_model()
 
@@ -17,6 +20,22 @@ class CreateAccountForm(forms.ModelForm):
         self.fields["password"].required = True
         self.fields["first_name"].required = True
         self.fields["last_name"].required = True
+
+    def save(self, commit=True):
+        user = super(CreateAccountForm, self).save(commit=False)
+
+        # Check if the ID is already used
+        with transaction.atomic():
+            max_id = self.Meta.model.objects.aggregate(max_id=Max('id'))['max_id'] or 0
+            while self.Meta.model.objects.filter(id=max_id + 1).exists():
+                max_id += 1
+            user.id = max_id + 1
+
+        if commit:
+            user.save()
+            self.save_m2m()  # Required for saving ManyToMany relations
+
+        return user
 
 class EditAccountForm(forms.ModelForm):
     class Meta:
