4f29da6ff92e9515c0236c0f801ee44dd8427779
ceylanbagci <hotbird3005@hotmail.com>
2024-08-01 10:39:10 -0700

    migrate starts

--- migration/views.py
+++ migration/views.py
@@ -3037,7 +3037,7 @@ def check_patient(request):
     from .migrate_dump import MigrateDump
     # SampleLib.objects.filter().delete()
     # MigrateDump.register_patients()
-    MigrateDump.register_areas()
+    MigrateDump.register_nuc_acids()
     # for na in NucAcids.objects.all():
     #     print(na)
     #     obj, created = AREA_NA_LINK.objects.get_or_create(nucacid=na,area=na.area)
