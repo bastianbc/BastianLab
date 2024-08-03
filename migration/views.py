301a354dc07fcc15d14d14f282aa517eedf51763
ceylanbagci <hotbird3005@hotmail.com>
2024-08-03 12:50:37 -0700

    migrate starts

--- migration/views.py
+++ migration/views.py
@@ -3626,6 +3626,8 @@ def find_path_seq_run_for_file_sets(request):
                 (r'.*IYEH(\d+).*', "", "_saved__IYEH_"),
                 (r'.*Hunter_RNAseq.*', "", "saved__Hunter_RNAseq_")
                 (r'.*VisiumCytAssit_MW.*', "", "saved__VisiumCytAssit_MW_")
+                (r'.*Public ChIp-seq data.*', "", "saved__Public ChIp-seq data_")
+                (r'.*VisiumCytAssit_MW.*', "", "saved__VisiumCytAssit_MW_")
             ]
 
             for pattern, prefix, message in patterns:
