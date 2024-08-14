8202e6be4253d259c62e4bc451af16d7d875d6b8
ceylanbagci <hotbird3005@hotmail.com>
2024-08-13 18:27:44 -0700

    start analysis sheet

--- sequencingfile/forms.py
+++ sequencingfile/forms.py
@@ -8,7 +8,7 @@ class SequencingFileForm(BaseForm, forms.ModelForm):
         fields = "__all__"
 
     def __init__(self, *args, **kwargs):
-        super(SequencingFileSetForm, self).__init__(*args, **kwargs)
+        super(SequencingFileForm, self).__init__(*args, **kwargs)
         for visible in self.visible_fields():
             if type(visible.field) == forms.fields.TypedChoiceField or type(visible.field) == forms.models.ModelChoiceField or type(visible.field) == forms.ChoiceField:
                 visible.field.widget.attrs["data-control"] = "select2"
