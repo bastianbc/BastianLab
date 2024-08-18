22253ea68e5b315992dcb30b0b75042e9c17eaf3
bazo <sitivcabs@yahoo.com>
2024-08-19 01:00:49 +0200

    bug fixed in analysis run name generate

---
+++ analysisrun/storage.py
@@ -0,0 +1,25 @@
+from django.core.files.storage import FileSystemStorage
+from django.conf import settings
+import os
+
+class CustomFileSystemStorage(FileSystemStorage):
+    def __init__(self, *args, **kwargs):
+        # Default directory to store files
+        kwargs['location'] = settings.SMB_DIRECTORY
+        super(CustomFileSystemStorage, self).__init__(*args, **kwargs)
+
+    def get_available_name(self, name, max_length=None):
+        # Override to prevent name collision
+        if self.exists(name):
+            name = self.get_alternative_name(name)
+        return super(CustomFileSystemStorage, self).get_available_name(name, max_length)
+
+    def get_alternative_name(self, name):
+        # Generate a unique name for the file if it already exists
+        base, ext = os.path.splitext(name)
+        counter = 1
+        new_name = f"{base}_{counter}{ext}"
+        while self.exists(new_name):
+            counter += 1
+            new_name = f"{base}_{counter}{ext}"
+        return new_name
