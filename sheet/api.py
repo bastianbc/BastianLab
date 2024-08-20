5b0b21b0f662c067fa70b39125c7e8c52219dec8
ceylanbagci <hotbird3005@hotmail.com>
2024-08-19 19:28:24 -0700

    start analysis sheet

--- sheet/api.py
+++ sheet/api.py
@@ -51,9 +51,8 @@ def _get_authorizated_queryset(seq_runs):
                 SampleLib.objects.filter(
                     na_sl_links__nucacid__area_na_links__area__area_type='normal',
                     na_sl_links__nucacid__area_na_links__area__block__patient=OuterRef(
-                        "na_sl_links__nucacid__area_na_links__area__block__patient")
-                ).filter(
-                    Exists(matching_normal_sl_subquery)
+                        "na_sl_links__nucacid__area_na_links__area__block__patient"),
+                    sl_cl_links__captured_lib__cl_seql_links__sequencing_lib__sequencing_runs__is_null=False
                 ).values('name')[:1]
             ),
             output_field=CharField()
