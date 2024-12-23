f0990b938d2660cc4e6b3b0241b4feef276ffb80
ceylanbagci <hotbird3005@hotmail.com>
2024-12-23 09:19:54 -0800

    db migration

--- migration/db_migration.py
+++ migration/db_migration.py
@@ -4,42 +4,42 @@ from django.apps import apps
 
 
 class Command(BaseCommand):
-    help = "Copy Buffer data from labdb to labdbproduction using the slug field."
+    help = "Copy Method data from labdb to labdbproduction using the slug field."
 
     def handle(self, *args, **kwargs):
         source_db = "labdb"  # Source database
         target_db = "default"  # Target database (labdbproduction)
 
-        self.stdout.write("Starting data copy for Buffer...")
+        self.stdout.write("Starting data copy for Method...")
 
         try:
-            # Get the Buffer model
-            Buffer = apps.get_model("buffer", "Buffer")
+            # Get the Method model
+            Method = apps.get_model("method", "Method")
 
-            # Fetch all Buffer objects from the source database
-            source_buffers = Buffer.objects.using(source_db).all()
+            # Fetch all Method objects from the source database
+            source_methods = Method.objects.using(source_db).all()
 
             with transaction.atomic(using=target_db):
-                for buffer in source_buffers:
+                for method in source_methods:
                     try:
-                        # Check if the Buffer already exists in the target database using the slug
-                        Buffer.objects.using(target_db).get(slug=buffer.slug)
+                        # Check if the Method already exists in the target database using the slug
+                        Method.objects.using(target_db).get(slug=method.slug)
                         continue  # Skip if it already exists
-                    except Buffer.DoesNotExist:
-                        # Create the Buffer in the target database
-                        Buffer.objects.using(target_db).create(
-                            name=buffer.name,
-                            slug=buffer.slug,
+                    except Method.DoesNotExist:
+                        # Create the Method in the target database
+                        Method.objects.using(target_db).create(
+                            name=method.name,
+                            slug=method.slug,
                         )
 
             self.stdout.write(
-                f"Successfully copied {len(source_buffers)} Buffer objects from {source_db} to {target_db}."
+                f"Successfully copied {len(source_methods)} Method objects from {source_db} to {target_db}."
             )
 
         except Exception as e:
-            self.stdout.write(f"Error copying data for Buffer: {e}")
+            self.stdout.write(f"Error copying data for Method: {e}")
 
-        self.stdout.write("Data copy for Buffer completed.")
+        self.stdout.write("Data copy for Method completed.")
 
 
 def run():
