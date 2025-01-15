e917bff63469a8d0eea04645f141871a3e4b6e9a
ceylanbagci <hotbird3005@hotmail.com>
2025-01-15 13:50:28 -0800

    backgrounds changed

--- migration/views.py
+++ migration/views.py
@@ -3845,7 +3845,7 @@ def call_import_variants(request):
 def import_genes(request):
     from cns.models import Cns
     from variant.models import VariantFile
-    q = (Q(variantfile_cns__isnull=False) & Q(name__icontains=".bintest.") & Q(name__icontains=".call."))
+    q = Q(Q(variantfile_cns__isnull=False) & Q(name__icontains=".bintest.") & Q(name__icontains=".call."))
     for file in VariantFile.objects.filter(q):
         print(file.name)
 
