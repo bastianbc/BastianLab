b2025b55007dd7e54596d382d246c1f8a99832bc
ceylanbagci <hotbird3005@hotmail.com>
2025-02-03 15:29:45 -0800

    SEQUENCING_FILES_SOURCE_DIRECTORY added

--- cns/helper.py
+++ cns/helper.py
@@ -9,33 +9,34 @@ from io import BytesIO
 
 def generate_graph(ar_name,file_path):
  # CSV verilerini okuma
-    df = pd.read_csv(file_path)
-    
-    # Örneğin, sadece 'chr11' kromozomuna ait verileri filtreleyelim
-    df_subset = df[df['chromosome'] == 'chr11']
-    
-    # Belirli örnekleri seçelim
-    sample_subset = df_subset['start'].unique()[:5]  # İlk 5 örnek
-    df_subset = df_subset[df_subset['start'].isin(sample_subset)]
-    
-    # Plot the limited subset
-    plt.figure(figsize=(10, 6))
-    
-    for sample in sample_subset:
-        sample_data = df_subset[df_subset['start'] == sample]
-        plt.plot(sample_data['start'], sample_data['log2'], label=sample)
+   print("^"*100, file_path)
+   df = pd.read_csv(file_path)
 
-    plt.xlabel('Genomic Position')
-    plt.ylabel('Log2 Ratio')
-    plt.title('Chromosome 11 Segments for Identified Samples')
-    plt.legend()
+   # Örneğin, sadece 'chr11' kromozomuna ait verileri filtreleyelim
+   df_subset = df[df['chromosome'] == 'chr11']
 
-    buffer = BytesIO()
-    canvas = pylab.get_current_fig_manager().canvas
-    canvas.draw()
-    pilImage = Image.frombytes("RGB", canvas.get_width_height(), canvas.tostring_rgb())
-    pilImage.save(buffer, "PNG")
+   # Belirli örnekleri seçelim
+   sample_subset = df_subset['start'].unique()[:5]  # İlk 5 örnek
+   df_subset = df_subset[df_subset['start'].isin(sample_subset)]
 
-    return base64.b64encode(buffer.getvalue()).decode("utf-8")
+   # Plot the limited subset
+   plt.figure(figsize=(10, 6))
+
+   for sample in sample_subset:
+      sample_data = df_subset[df_subset['start'] == sample]
+      plt.plot(sample_data['start'], sample_data['log2'], label=sample)
+
+   plt.xlabel('Genomic Position')
+   plt.ylabel('Log2 Ratio')
+   plt.title('Chromosome 11 Segments for Identified Samples')
+   plt.legend()
+
+   buffer = BytesIO()
+   canvas = pylab.get_current_fig_manager().canvas
+   canvas.draw()
+   pilImage = Image.frombytes("RGB", canvas.get_width_height(), canvas.tostring_rgb())
+   pilImage.save(buffer, "PNG")
+
+   return base64.b64encode(buffer.getvalue()).decode("utf-8")
 
    
