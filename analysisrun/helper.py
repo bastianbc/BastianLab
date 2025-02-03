f59a6252404a01e7b39c64d9b975f73a115ba838
ceylanbagci <hotbird3005@hotmail.com>
2025-02-03 15:13:06 -0800

    SEQUENCING_FILES_SOURCE_DIRECTORY added

--- analysisrun/helper.py
+++ analysisrun/helper.py
@@ -11,7 +11,9 @@ BASE_PATH = settings.VARIANT_FILES_SOURCE_DIRECTORY
 def handle_variant_file(ar_name, variant_path):
     folder_path = find_folder(BASE_PATH, ar_name, variant_path)
     print(ar_name, BASE_PATH, variant_path)
+    folder_path = os.path.join(BASE_PATH, ar_name, variant_path)
     print("*"*100, folder_path)
+
     if folder_path:
         full_path = os.path.join(folder_path, 'output')
         file_path = search_cns_file(full_path)
