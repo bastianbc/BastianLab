0e957ebdea1693e958db1df1671beac9f66ecb6b
ceylanbagci <hotbird3005@hotmail.com>
2025-02-19 17:42:48 -0800

    gene detail

--- migration/variants_import.py
+++ migration/variants_import.py
@@ -290,6 +290,7 @@ def create_gene_detail(gene_detail, row_gene, g_variant, func):
 
 def create_c_and_p_variants(g_variant, aachange, func, gene_detail, filename, row_gene):
     logger.debug(f"Creating C and P variants for aachange: {aachange}")
+    print("aachange: ", aachange, " gene_detail: ", gene_detail)
     entries = aachange.split(',')
     if any(aachange in invalid for invalid in ["UNKNOWN", "."]):
         print("%%%")
