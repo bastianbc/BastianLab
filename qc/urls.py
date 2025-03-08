3574edd57edd0d6870370540146abd4fac7e28b1
bazo <sitivcabs@yahoo.com>
2025-03-08 01:35:40 +0100

    bug fixed

--- qc/urls.py
+++ qc/urls.py
@@ -3,7 +3,7 @@ from . import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
-    path("", views.analysisruns, name="analysisruns"),
+    path("", views.sample_qcs, name="sample_qcs"),
     path("filter_sampleqc", views.sample_qcs, name="filter-sampleqc"),
     path('import_qc/<str:ar_name>', views.import_qc, name='import_qc'),
 
