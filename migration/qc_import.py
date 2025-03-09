7cc41e3365e1713567b91794d5b2b12dd22ae47c
ceylanbagci <hotbird3005@hotmail.com>
2025-03-09 10:11:36 -0700

    qc start

--- migration/qc_import.py
+++ migration/qc_import.py
@@ -14,7 +14,7 @@ import re
 import logging
 from pathlib import Path
 from gene.models import Gene
-from qc.helper import parse_dup_metrics
+from qc.helper import parse_dup_metrics, parse_hs_metrics
 from qc.models import SampleQC
 from analysisrun.models import AnalysisRun
 
@@ -160,23 +160,44 @@ def create_qc_sample(metrics, file):
         sample_lib=get_sample_lib(file),
         analysis_run=AnalysisRun.objects.get(name="AR_ALL"),
         sequencing_run=get_sequencing_run(file),
-        unpaired_reads_examined=metrics['unpaired_reads_examined'],
-        read_pairs_examined=metrics['read_pairs_examined'],
-        secondary_or_supplementary_rds=metrics['secondary_or_supplementary_rds'],
-        unmapped_reads=metrics['unmapped_reads'],
-        unpaired_read_duplicates=metrics['unpaired_read_duplicates'],
-        read_pair_duplicates=metrics['read_pair_duplicates'],
-        read_pair_optical_duplicates=metrics['read_pair_optical_duplicates'],
-        percent_duplication=metrics['percent_duplication'],
-        estimated_library_size=metrics['estimated_library_size'],
+        unpaired_reads_examined=metrics.get('unpaired_reads_examined', None),
+        read_pairs_examined=metrics.get('read_pairs_examined', None),
+        secondary_or_supplementary_rds=metrics.get('secondary_or_supplementary_rds', None),
+        unmapped_reads=metrics.get('unmapped_reads', None),
+        unpaired_read_duplicates=metrics.get('unpaired_read_duplicates', None),
+        read_pair_duplicates=metrics.get('read_pair_duplicates', None),
+        read_pair_optical_duplicates=metrics.get('read_pair_optical_duplicates', None),
+        percent_duplication=metrics.get('percent_duplication', None),
+        estimated_library_size=metrics.get('estimated_library_size', None),
+        pct_off_bait=metrics.get('pct_off_bait', None),
+        mean_bait_coverage=metrics.get('mean_bait_coverage', None),
+        mean_target_coverage=metrics.get('mean_target_coverage', None),
+        median_target_coverage=metrics.get('median_target_coverage', None),
+        pct_target_bases_1x=metrics.get('pct_target_bases_1x', None),
+        pct_target_bases_2x=metrics.get('pct_target_bases_2x', None),
+        pct_target_bases_10x=metrics.get('pct_target_bases_10x', None),
+        pct_target_bases_20x=metrics.get('pct_target_bases_20x', None),
+        pct_target_bases_30x=metrics.get('pct_target_bases_30x', None),
+        pct_target_bases_40x=metrics.get('pct_target_bases_40x', None),
+        pct_target_bases_50x=metrics.get('pct_target_bases_50x', None),
+        pct_target_bases_100x=metrics.get('pct_target_bases_100x', None),
+        at_dropout=metrics.get('at_dropout', None),
+        gc_dropout=metrics.get('gc_dropout', None),
     )
 
 def parse_parse_dup_metrics():
-    dup_metrics = VariantFile.objects.filter(type='qc', name__icontains='dup_metrics')
+    # dup_metrics = VariantFile.objects.filter(type='qc', name__icontains='dup_metrics')
+    # for file in dup_metrics:
+    #     path = os.path.join(settings.SMB_DIRECTORY_SEQUENCINGDATA,file.directory,file.name)
+    #     metrics = parse_dup_metrics(path)
+    #     create_qc_sample(metrics, file.directory.split('/')[-1])
+
+    dup_metrics = VariantFile.objects.filter(type='qc', name__icontains='hs_metrics')
     for file in dup_metrics:
         path = os.path.join(settings.SMB_DIRECTORY_SEQUENCINGDATA,file.directory,file.name)
-        metrics = parse_dup_metrics(path)
+        metrics = parse_hs_metrics(path)
         create_qc_sample(metrics, file.directory.split('/')[-1])
+        # create_qc_sample(metrics, file.directory.split('/')[-1])
 
         # print(path, metrics)
 
