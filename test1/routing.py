12770d537fde4bc110dab2eb7fe368a2c874eef9
ceylanbagci <hotbird3005@hotmail.com>
2025-03-17 20:41:37 -0700

    notification

---
+++ test1/routing.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from notification.consumers import JobNotificationConsumer
+
+websocket_urlpatterns = [
+    path("ws/notifications/", JobNotificationConsumer.as_asgi()),
+]
