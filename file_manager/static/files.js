27907d873b12d129d8a1ae3c884ebf6b76a5e18e
ceylanbagci <hotbird3005@hotmail.com>
2025-03-28 20:10:56 -0700

    setting redis testing file js

--- file_manager/static/files.js
+++ file_manager/static/files.js
@@ -252,8 +252,11 @@ var KTDatatablesServerSide = function () {
             setTimeout(function() {
                 console.log("reloadDatatableWithSubDir");
                 dt.ajax.url(`/file_manager/filter_files?exact_dir=${exact_dir}`).load(function(json) {
+                    console.log("1"*20);
                     dt.columns.adjust().draw(); // Ensures redrawing of the table
+                    console.log("2"*20);
                     updateBreadcrumb(exact_dir);
+                    console.log("3"*20);
                 });
             }, 5000); // 5000 milliseconds = 5 seconds
         }
@@ -316,10 +319,11 @@ var KTDatatablesServerSide = function () {
 
         // Then add a new listener at the document level
         $(document).on('click', '.table .reload-subdir-link', function() {
-            console.log("addReloadSubDirListeners");
+
             var self = this;
             setTimeout(function() {
                 var subDir = self.getAttribute('data-subdir');
+                console.log("addReloadSubDirListeners");
                 KTDatatablesServerSide.reloadDatatableWithSubDir(subDir);
             }, 5000);
         });
