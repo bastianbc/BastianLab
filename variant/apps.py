4aed17126ffe37bdbf8d967d5b0b7f3dbe181242
bazo <sitivcabs@yahoo.com>
2025-04-22 01:47:26 +0200

    new structure for variants by area

--- variant/apps.py
+++ variant/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
 
-
 class VariantConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'variant'
+
+    def ready(self):
+        import variant.signals  # Initials the signals
