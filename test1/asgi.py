86a6e7ee54a675cf5f8ddd46318383290d1583d2
ceylanbagci <hotbird3005@hotmail.com>
2025-05-12 17:18:29 -0700

    variant alias

--- test1/asgi.py
+++ test1/asgi.py
@@ -6,7 +6,6 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/dev/howto/deployment/asgi/
 """
-
 import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
@@ -16,7 +15,11 @@ from notification.routing import websocket_urlpatterns
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'test1.settings')
 
 application = ProtocolTypeRouter({
+    # HTTP requests are handled by Django’s ASGI application
     "http": get_asgi_application(),
+
+    # WebSocket connections go through the Auth middleware
+    # into your URLRouter (notification.routing.websocket_urlpatterns)
     "websocket": AuthMiddlewareStack(
         URLRouter(websocket_urlpatterns)
     ),
