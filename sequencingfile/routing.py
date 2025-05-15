6c722ede7bca1d255067e0802d022e9847b981ba
ceylanbagci <hotbird3005@hotmail.com>
2025-05-14 20:07:01 -0700

    'path.to.log_streaming.middleware.AsyncLogStreamMiddleware',

--- sequencingfile/routing.py
+++ sequencingfile/routing.py
@@ -3,5 +3,5 @@ from django.urls import re_path
 from .views import LogStreamerConsumer
 
 websocket_urlpatterns = [
-    re_path(r'ws/logs/$', LogStreamerConsumer.as_asgi()),
+    re_path(r'ws/logs', LogStreamerConsumer.as_asgi()),
 ]
