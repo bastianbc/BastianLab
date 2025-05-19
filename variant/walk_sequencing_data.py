01bef8fde7b7fdad701c99a177984a94ce4fe664
ceylanbagci <hotbird3005@hotmail.com>
2025-05-19 11:02:30 -0700

    Importing Variant Files

--- variant/walk_sequencing_data.py
+++ variant/walk_sequencing_data.py
@@ -29,5 +29,5 @@ def create_file_tree(root_dir_list, file_tree_file):
                         continue  # skip non-matching files
                     rel_dir = root.replace(settings.SMB_DIRECTORY_SEQUENCINGDATA, "")
                     var_file, _ = VariantFile.objects.get_or_create(name=fname, directory=rel_dir)
-                    print(f"{rel_dir[:-3]}-->{fname[:3]}\n --> {var_file}")
+                    print(f"{rel_dir[:3]}-->{fname[:3]}\n --> {var_file}")
                     f.write(f"{rel_dir}-->{fname}\n")
