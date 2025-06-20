6653102a4b521a62f4583ae227d20cd8559f225c
ceylanbagci <hotbird3005@hotmail.com>
2025-06-20 11:10:59 -0700

    ceate file tree

--- migration/file_tree.py
+++ migration/file_tree.py
@@ -1,18 +1,14 @@
 import os
-from pathlib import Path
 
-def create_file_tree(root_dirs:list, file_tree_file):
-    with open(file_tree_file, 'w') as f:
-        for root_dir in root_dirs:
+def create_file_tree():
+    print("start")
+    root_dir_list = ['/mnt/sequencingdata']
+    file_tree_output_file = 'file_tree_06_20_sequencingdata.txt'
+
+    with open(file_tree_output_file, 'w') as f:
+        for root_dir in root_dir_list:
             for root, dirs, files in os.walk(root_dir):
-                if "BastianLab/venv" in root or "BastianLab/.git" in root:
-                    continue
-                f.write('root:{} dirs:{} files: {}\n'.format(root, dirs, files))
-
-root_directory = ['/Users/cbagci/Documents/BastianLab']
-
-file_tree_output_file = 'bastianlab_file_tree.txt'
-
-# Call the function to create the file tree
-# create_file_tree(root_directory, file_tree_output_file)
-create_file_tree(root_directory, file_tree_output_file)
+                for file in files:
+                    full_path = os.path.join(root, file)
+                    f.write(full_path + "\n")
+    print("finish")
