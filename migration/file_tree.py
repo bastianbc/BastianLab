5cccd0c1089ae932e0856f7f332639253eff473e
ceylanbagci <hotbird3005@hotmail.com>
2025-06-20 11:41:15 -0700

    ceate file tree

--- migration/file_tree.py
+++ migration/file_tree.py
@@ -3,14 +3,13 @@ from django.conf import settings
 
 def create_file_tree():
     print("start")
-    root_dir_list = ['/mnt/sequencingdata']
+    root_dir = '/mnt/sequencingdata/ProcessedData'
     file_tree_output_file = 'file_tree_06_20_sequencingdata.txt'
     file_tree_output_file = os.path.join(settings.MEDIA_ROOT, 'file_tree_06_20_sequencingdata.txt')
 
     with open(file_tree_output_file, 'w') as f:
-        for root_dir in root_dir_list:
-            for root, dirs, files in os.walk(root_dir):
-                for file in files:
-                    full_path = os.path.join(root, file)
-                    f.write(full_path + "\n")
+        for root, dirs, files in os.walk(root_dir):
+            for file in files:
+                full_path = os.path.join(root, file)
+                f.write(full_path + "\n")
     print("finish")
