57e5594a86a332289f5de4c89d8c11c967c98815
ceylanbagci <hotbird3005@hotmail.com>
2025-06-22 05:21:47 -0700

    ceate file tree

--- migration/file_tree.py
+++ migration/file_tree.py
@@ -4,12 +4,7 @@ from sequencingfile.models import SMBDirectory
 
 def create_file_tree():
     print("start")
-    root_dirs = ['/mnt/labshare/BastianRaid-01/UCSF/Lab Maintenance',
-                '/mnt/labshare/BastianRaid-01/UCSF/Lab Protocols',
-                '/mnt/labshare/BastianRaid-01/UCSF/NexusProjects',
-                '/mnt/labshare/BastianRaid-01/UCSF/Plasmids',
-                '/mnt/labshare/BastianRaid-01/UCSF/Primers',
-                 '/mnt/labshare/BastianRaid-01/UCSF/Reference Sequences']
+    root_dirs = ['/mnt/labshare/BastianRaid-02']
     for dir in root_dirs:
         for root, dirs, files in os.walk(dir):
             for file in files:
