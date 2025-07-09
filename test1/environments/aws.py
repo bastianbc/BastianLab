c877c12375ca22b185baa3c86b79807a50c6718f
ceylanbagci <hotbird3005@hotmail.com>
2025-07-08 17:07:34 -0700

    DatabaseCredentialMiddleware

--- test1/environments/aws.py
+++ test1/environments/aws.py
@@ -110,7 +110,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'test1.db_connection_error.DatabaseCredentialMiddleware',
     # 'core.log_streaming.AsyncLogStreamMiddleware'
 ]
 
