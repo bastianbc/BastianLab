92fddeb7f03c29dc3148bf63cf41d5ebb8264387
ceylanbagci <hotbird3005@hotmail.com>
2025-09-13 11:54:10 -0700

    get_sequencing_files

--- sequencingrun/views.py
+++ sequencingrun/views.py
@@ -194,7 +194,7 @@ def add_async(request):
         return JsonResponse({"success":False, "message": str(e)})
 
 def get_sequencing_files(request, id):
-    execute_mount_script()
+    # execute_mount_script()
     try:
         sequencing_run = SequencingRun.objects.get(id=id)
         sample_libs = SampleLib.objects.filter(sl_cl_links__captured_lib__cl_seql_links__sequencing_lib__sequencing_runs=sequencing_run).distinct().order_by('name')
